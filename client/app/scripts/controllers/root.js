4fee386eda2dc772a523e5ddf3de7409b70003ee
Ricardo Romaní Trillo <raimangsxr@gmail.com>
2016-05-07 19:39:41 +0200

    Cambiado el host con el que se invoca al API REST del backend.

--- client/app/scripts/controllers/root.js
+++ client/app/scripts/controllers/root.js
@@ -11,8 +11,8 @@ angular.module('deportesOnApp')
   .controller('RootCtrl', function ($rootScope) {
     $rootScope.config = {
       protocol: 'http',
-      host: '192.168.1.113',
-      port: 3000
+      host: 'endirecto.chickenkiller.com',
+      port: 80
     }
     $rootScope.setNavActive = function(idNewActive){
       var navOldActive = angular.element(document.querySelector('.active'));
